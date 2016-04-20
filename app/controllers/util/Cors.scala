87647c88c8dd5138c1a486243cb40acd02bbd888
Leo <mirandaleandro@gmail.com>
2016-04-20 00:13:18 -0600

    tweaking cors

--- app/controllers/util/Cors.scala
+++ app/controllers/util/Cors.scala
@@ -9,10 +9,9 @@ import play.api.mvc.Action
 trait Cors {
   def headers = List(
     "Access-Control-Allow-Origin" -> "*",
-    "Access-Control-Expose-Headers" -> "*",
-    "Access-Control-Allow-Methods" -> "GET, POST, OPTIONS, DELETE, PUT",
+    "Access-Control-Allow-Methods" -> "*",
     "Access-Control-Max-Age" -> "3600",
-    "Access-Control-Allow-Headers" -> "Origin, Content-Type, Accept, Authorization",
+    "Access-Control-Allow-Headers" -> "Access-Control-Allow-Origin,Origin,X-Requested-With, Content-Type, Accept, Authorization",
     "Access-Control-Allow-Credentials" -> "true"
   )
 
